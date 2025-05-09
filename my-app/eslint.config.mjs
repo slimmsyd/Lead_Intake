7984b0d340f68a522e1c215c7ec8c225bc267eaa
slimmsyd <ssanderss444@gmail.com>
2025-05-09 19:11:18 -0400

    u[dated teh ignore

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/components/lead-intake/*"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
